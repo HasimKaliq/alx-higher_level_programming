4fc0981dcc2f487b7c61fb826fae0e1fa8a13d71
hasim kaliq <hasimkaliq@gmail.com>
2022-12-05 18:01:59 -0800

    Made a lot of changes to the script 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ last_index_val = int(num_str[len(num_str)-1])
 if (number > 0):
     if (last_index_val > 5):
         print(f"Last digit of {number} is {last_index_val} \
-        and is greater than 5")
+and is greater than 5")
 if (last_index_val == 0):
     print(f"Last digit of {number} is {last_index_val} and is 0")
 if (number < 0):
@@ -14,4 +14,4 @@ if (number < 0):
         print(f"Last digit of {number} is {last_index_val} and is 0")
     elif(last_index_val != 0):
         print(f"Last digit of {number} is {-1 * last_index_val} and \
-        is less than 6 and not 0")
+is less than 6 and not 0")
