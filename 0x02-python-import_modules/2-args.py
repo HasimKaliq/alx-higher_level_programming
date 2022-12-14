3aa066090496d8b1bd592c2dec7f6f58650c1816
hasim kaliq <hasimkaliq@gmail.com>
2022-12-14 13:45:32 -0800

    Added the third installment

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_len = len(sys.argv)
+    if (len(sys.argv == 1):
+        print("{} arguments.", len(sys.argv) - 1)
+    else:
+        for arg in range(1. len(sys.argv)):
+            if ((len(sys.argv) - 1) == 1):
+                print("{}: {}".format(len(sys.argv) - 1, sys.argv[arg]))
+            else:
+                print("{} {}".format(len(sys.argv) - 1, sys.argv[arg]))
