cbbac0fa7153d86e0a25d78f42a4162bafff503a
hasim kaliq <hasimkaliq@gmail.com>
2022-12-13 16:56:55 -0800

    Called the function add from with in the print function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,7 @@
 from add_0 import add
 a = 1
 b = 2
-sum = add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+print("{} + {} = {}".format(a, b, add(a, b)))
 if __name__ == "__main__":
     import sys
     add(int(sys.argv[1]))
