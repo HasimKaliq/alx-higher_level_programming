239b323353c2166a7f2e8b73f5ab84ea6e9129de
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-18 14:47:59 -0800

    fixexd a bug

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,7 @@ def safe_print_integer(value):
     try:
         if (int(value)):
             print("{:d}".format(value))
-
+        print("")
     except ValueError:
         return False
-    print("")
     return True
