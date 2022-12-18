b91d3e574fdae5e15cca3e904841bc994096384f
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-18 14:19:34 -0800

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
+from regex import F
 def safe_print_list(my_list=[], x=0):
-    try:
         i = 0
-        while(my_list[i]):
-            i+=1
-    except IndexError:
-       pass
-    
-    try:
-        if (x > i or x < i):
-            raise IndexError
-        else:
-            for index in  range(x):
-                print("{}".format(my_list[index]),end="")
-            print("\n")
-    except IndexError:
-        for index in  range(i):
-            print("{}".format(my_list[index]),end="")
-        print("\n")    
-    return (i)
+        for index in range(x):
+            try:
+                print("{}".format(my_list[index]), end="")
+                i+=1
+            except IndexError:
+                break
+        print("")
+        return (i)
+my_list = [1, 2, 3, 4, 5]
