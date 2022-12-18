8964ecf3c79ba708f34c48a9f1d1e4eedcfb0fda
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-18 14:43:21 -0800

    Added the code to the script

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    try:
+        if (int(value)):
+            print("{:d}".format(value))
+
+    except ValueError:
+        return False
+    print("")
+    return True
