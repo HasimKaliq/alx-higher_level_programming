8fbc7f19f8594fcb4100b7a12b7e65fd3725ed0d
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-20 14:57:02 -0800

    added the script for division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,14 +8,3 @@ def safe_print_division(a, b):
         return None
     finally:
         print("Inside result: {:.1f}".format(result))
-
-
-a = "SA"
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
