6b74df31d2e5b867b6278b1951962feb480af2a5
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-20 14:24:28 -0800

    changes made

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
               print("{:d}".format(my_list[index]),end="")
               size+=1  
         print("")
-        return size
     except (TypeError, IndexError, ValueError):
         pass
+    return (size)
