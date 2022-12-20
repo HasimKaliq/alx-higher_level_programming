b93e2e611284daafe7807a0378326670be760aaf
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-20 12:53:36 -0800

    Reduced the white space

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if (int(value)):
-            print("{:d}\n".format(value))
-
-    except ValueError:
+        print("{:d}".format(value))
+        return (True)
+    except (ValueError, TypeError,IndexError):
         return (False)
-    
-    return (True)
