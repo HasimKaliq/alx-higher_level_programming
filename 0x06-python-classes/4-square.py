cc3eab89cca668f1e767c66ba085ba2fed47d8fd
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-25 15:28:35 -0800

    added the logic for 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """This a class, that has the private Variable size"""
+    def __init__(self, size=0):
+        """
+        Args:
+            size(int): The size for building the square
+        """
+        self.__size = size
+        try:
+            """
+            Raises:
+                TypeError: If the argunment passed is not an int.
+                ValueError: If the argunment passed is not >= 0.
+            """
+            if(type(self.__size) is not int):
+                raise TypeError
+            elif(type(self.__size) is int) and self.__size < 0:
+                raise ValueError
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be >= 0")
+
+    def size(self):
+        """The size method returns initaial size
+        Returns:
+            int: the initial passed value
+        """
+        return self.__size
+
+    def size(self, value):
+        """The size method take an extra parameter,
+            the value will be used to update self__size
+        Args:
+            value(int): the argument expected
+            """
+        self.__size = value
+        try:
+            if(type(self.__size) is not int):
+                raise TypeError
+            elif(type(self.__size) is int) and self.__size < 0:
+                raise ValueError
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """The area fuction once the checks are valid
+            Will return the square of self.__size.
+
+        Returns:
+            int: the square of the number self.__size.
+        """
+        return(self.__size * self.__size)
