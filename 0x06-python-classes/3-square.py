2ba17b08dbe29cf5ea48804820262d13abf7fcde
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-25 14:40:38 -0800

    updated the code

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """This a class, that has the private Variable size"""
+    def __init__(self, size=0):
+        """
+        Args:
+            size(int): The size for building the square
+        """
+        self.__size = size
+        try:
+            """
+            Raises:
+                TypeError: If the argunment passed is not an int.
+                ValueError: If the argunment passed is not >= 0.
+            """
+            if(type(self.__size) is not int):
+                raise TypeError
+            elif(type(self.__size) is int) and self.__size < 0:
+                raise ValueError
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return (self.__size * self.__size)
