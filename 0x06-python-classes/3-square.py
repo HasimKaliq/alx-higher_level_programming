bf7cdba1bffd0a2411848b545480de8a9bd50c23
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-25 14:44:47 -0800

    updated the code

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -25,6 +25,5 @@ class Square:
             raise TypeError("size must be an integer")
         except ValueError:
             raise ValueError("size must be >= 0")
-    
     def area(self):
         return (self.__size * self.__size)
