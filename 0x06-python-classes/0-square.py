2e95cc92862df919b01f736304c6d59f19346644
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-24 19:02:21 -0800

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """This a class, that does nothing, 
+    """This a class, that does nothing,
         thus the pass keyword can be passed to indicate
         This is an empty class, 
         implementation may be provided in the future."""
