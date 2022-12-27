6b177b08e395f32727b802e50a4bb1752d26b377
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-27 14:36:58 -0800

    updated

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 
 
-"""If you're reading my code, I do apologize for my indulgence in details, that's who I am.
-    If I can't build it from scratch, it's not fun,  I always get a feeling of less fulfillment, when I have to call a method.
-    Take the code below I could have called the method reverse on the list but i'd loose the enjoyment of knowing how things work. 
-    maybe I give too much to details? or maybe it's bececause C made me do everything from scratch? either way, do enjoy!"""
+"""If you're reading my code,
+    I do apologize for my indulgence in details, that's who I am.
+    If I can't build it from scratch, it's not fun,
+    I always get a feeling of less fulfillment,
+    when I have to call a method.
+    Take the code below I could have called the method reverse on the list 
+    but i'd loose the enjoyment
+    of knowing how things work.
+    maybe I give too much to details?
+    or maybe it's bececause C made me do everything from scratch? either way,
+    do enjoy!"""
+
+
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 1:
         print("{:d}".format(my_list[0]))
