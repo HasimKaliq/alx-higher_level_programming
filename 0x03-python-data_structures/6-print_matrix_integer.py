b1e87a8a949b19e8e93a530be56245e31e67f511
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-27 18:35:53 -0800

    updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
+"""Here row -> Is the row of the matrix
+   Here val -> is the value in the current row
+"""
+
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for value in row:
-            print("{:d}".format(value), end=" ")
+    for row in range(len(matrix)):
+        for val in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][val]), end=" ")
         print()
