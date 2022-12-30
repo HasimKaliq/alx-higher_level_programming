1c8a5a88a07116c0d84286c13ed7f125b6c1bb71
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-30 13:37:24 -0800

    created the solution for 0-square

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    new_matix = [[], [], []]
+    idx = 0
+    for i in matrix:
+        for j in i:
+            square = j * j
+            new_matix[idx].append(square)
+        idx += 1
+    return new_matix
