7800e43e5a0a80c7b7e12419ce1a62713c22257d
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-30 14:25:59 -0800

    created the solution for 1-search_replace.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,11 +2,11 @@
 
 
 def square_matrix_simple(matrix=[]):
-    new_matix = [[], [], []]
+    new_matix = [ [], [], [] ]
     idx = 0
     for i in matrix:
         for j in i:
-            square = j * j
-            new_matix[idx].append(square)
+           square = j * j
+           new_matix[idx].append(square)
         idx += 1
     return new_matix
