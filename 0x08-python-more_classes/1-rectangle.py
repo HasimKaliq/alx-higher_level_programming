90e9be81623f88f7ec4c694d2d679b1ce90191bc
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-30 21:29:53 -0800

    Added the docstrings

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,11 +7,25 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-    """
+        """
     Args:
         width(int): the first parameter
         height(int): the second parameter
     """
+        if not isinstance(self.__width, int):
+            raise TypeError("width must be an integer")
+        elif self.__width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+        if not isinstance(self.__height, int):
+            raise TypeError("height must be an integer")
+        elif self.__height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
     @property
     def width(self):
         """A Getter method
