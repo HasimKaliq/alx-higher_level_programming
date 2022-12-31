1ff29788fa337de0611e96b055bbb55ef24f87c8
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-30 22:49:57 -0800

    Added the docstrings

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        self.number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -84,4 +84,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        self.number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
