099aff7b3bf014398bd15ed99b84424a1b70f043
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-30 22:24:38 -0800

    Added the docstrings

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,28 +3,17 @@
 
 
 class Rectangle:
-    """Constructs an object of type Rectangle"""
-    def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
-        """
-        Args:
-            width(int): the first parameter
-            height(int): the second parameter
-        """
-        if not isinstance(self.__width, int):
-            raise TypeError("width must be an integer")
-        elif self.__width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
+    """Represents the Rectangle class"""
 
-        if not isinstance(self.__height, int):
-            raise TypeError("height must be an integer")
-        elif self.__height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = height
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height 
 
     @property
     def width(self):
