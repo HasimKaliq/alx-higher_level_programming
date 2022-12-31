17409532640a8bfddc8d2183df38315834637307
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-30 22:56:19 -0800

    update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,20 +73,3 @@ class Square:
             for i in range(1, self.__size + 1):
                 print("#", end="")
             print()
-
-Square = __import__('6-square').Square
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-print(my_square_3.position)
-print("--")
