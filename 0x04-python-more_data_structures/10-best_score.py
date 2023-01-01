3e1f3d6ad5e5083ba9dd2e7e9648baa9d1e824c7
Hasim Kaliq <hasimkaliq@gmail.com>
2022-12-31 18:31:08 -0800

    Added the logic for 19

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a function that returns a key with the biggest integer value."""
+
+
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict):
+        return None
+    else:
+        dict_by_values = list(a_dictionary.values())
+        dict_by_values.sort()
+        largest_val = dict_by_values[len(dict_by_values)-1]
+        for key in a_dictionary.keys():
+            if a_dictionary[key] == largest_val:
+                return key
+            else:
+                return None
