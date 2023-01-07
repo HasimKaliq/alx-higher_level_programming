93a9c1e72faa3d0c78277a5b93b54066a88029c6
Hasim Kaliq <hasimkaliq@gmail.com>
2023-01-06 18:52:23 -0800

    Pushed the solution to 2-is_same

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+    """a function that check of instace
+    Args:
+        obj(instance) - Parameter 1
+        a_class(class) - Parameter 2
+    Return:
+        Bool: True if  instance
+              False if not
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
